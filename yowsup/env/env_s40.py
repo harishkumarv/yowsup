32fe038952267ec7266afe7aa406ed7711a84147
Harish Kumar V <xzntric@gmail.com>
2016-12-13 18:00:48 +0530

    Update env_s40.py

--- yowsup/env/env_s40.py
+++ yowsup/env/env_s40.py
@@ -37,4 +37,5 @@ class S40YowsupEnv(YowsupEnv):
             OS_VERSION = self.getOSVersion(),
             DEVICE_NAME = self.getDeviceName(),
             MANUFACTURER = self.getManufacturer()
+            print WHATSAPP_VERSION + ">>>>>>>>>>>>>>>>>>>>>>"
         )
